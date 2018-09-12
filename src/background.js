4e84b2eafad72ba027cfd1c2b20a18f68ef3cab9
Andrea Continella <andreacontinella@gmail.com>
2018-09-12 15:08:58 -0700

    correct endpoint + check status code

--- src/background.js
+++ src/background.js
@@ -8,33 +8,39 @@ chrome.webRequest.onBeforeRequest.addListener(
       console.log("[Truster] Checking " + info.url);
 
       // Get blacklist
-      var endpoint = "http://localhost/asd";
+      // TODO timeout if endpoint does not respond
+      var endpoint = "https://bucketsec.necst.it/api/blacklist";
       var xmlHttp = new XMLHttpRequest();
       xmlHttp.open("GET", endpoint, false);
       xmlHttp.setRequestHeader("Cache-Control", "no-cache");
       xmlHttp.send(null);
-      var blacklist = JSON.parse(xmlHttp.responseText);
 
-      // Get bucket name
-      var re = /http(s)?:\/\/([a-zA-Z0-9.\-\_]+?).s3(-.*?)?.amazonaws.com\//;
-      var bucketName;
+      if (xmlHttp.status == 200){
+        var blacklist = JSON.parse(xmlHttp.responseText);
 
-      var match = re.exec(info.url)[2];
-      if (match != null) {
-        bucketName = match;
+        // Get bucket name
+        var re = /http(s)?:\/\/([a-zA-Z0-9.\-\_]+?).s3(-.*?)?.amazonaws.com\//;
+        var bucketName;
+
+        var match = re.exec(info.url)[2];
+        if (match != null) {
+          bucketName = match;
+        } else{
+          re = /http(s)?:\/\/s3(-.*?)?.amazonaws.com\/([a-zA-Z0-9.\-\_]+?)\//;
+          match = re.exec(info.url)[3];
+          bucketName = match;
+        }
+
+        // Hash bucket name
+        var hashedBucketName = sha256(bucketName);
+
+        // Check if hashed bucket name is in the blacklist. If so, do no load the resource
+        if (blacklist.includes(hashedBucketName)){
+          console.log('[Truster] Untrusted resource blocked: ' + info.url);
+          return {redirectUrl: "javascript:"};
+        }
       } else{
-        re = /http(s)?:\/\/s3(-.*?)?.amazonaws.com\/([a-zA-Z0-9.\-\_]+?)\//;
-        match = re.exec(info.url)[3];
-        bucketName = match;
-      }
-      
-      // Hash bucket name
-      var hashedBucketName = sha256(bucketName);
-
-      // Check if hashed bucket name is in the blacklist. If so, do no load the resource
-      if (blacklist.includes(hashedBucketName)){
-        console.log('[Truster] Untrusted resource blocked: ' + info.url);
-        return {redirectUrl: "javascript:"};
+        console.log("[Truster] Error downloading blacklist");
       }
     }
   },
