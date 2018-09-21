06f1794167aa4de08f02394e778df040c7e69cd9
Marcello Pogliani <pogliamarci@hotmail.it>
2018-09-21 20:06:15 +0200

    Various fixes and improvements
    * Retain settings on options UI
    * Render options UI embedded in Chrome's settings
    * Use chrome.sync instead of localStorage for settings
    * Block requests (ERR:BLOCKED_BY_CLIENT) instead of redirecting
    * Add badge with number of blocked resources

--- src/background.js
+++ src/background.js
@@ -1,116 +1,158 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    // Check if request points to S3
-    var url = new URL(info.url);
-    var hostname = url.hostname;
-    var TSTHRESHOLD = 1000*60*60; // 1 hour
-    var blacklist;
-    var downloadBlacklist = false;
+var settings = {};
+var tabs = {};
 
-    if (hostname.endsWith("s3.amazonaws.com")){
-      console.log("[Truster] Checking " + info.url);
+installListeners = function() {
+  chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, {
+    urls: ["<all_urls>"]
+  },
+  ["blocking"]);
 
-      // Get blacklist
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+      var storageChange = changes[key];
+      if (key == 'truster_action' || key == 'truster_sendBucket') {
+        settings[key] = storageChange.newValue;
+      } 
 
-      // Check if blacklist is stored
-      if (localStorage.getItem("truster-blacklist") != null){
-        console.log("[Truster] Blacklist is cached");
-        var storedObj = JSON.parse(localStorage.getItem("truster-blacklist"));
-        var currTimestamp = new Date().getTime();
-        if (currTimestamp - storedObj.ts < TSTHRESHOLD){
-          blacklist = storedObj.blacklist;
-        } else{
-          // Cache expires
-          console.log("[Truster] Blacklist cached expired");
-          downloadBlacklist = true;
-        }
+  chrome.tabs.onRemoved.addListener(onTabRemoved);
+  chrome.tabs.onReplaced.addListener(onTabReplaced);
+}
+
+chrome.storage.sync.get({
+  truster_action : 'block',
+  truster_sendBucket : 'sendBucket'
+}, function(items) {
+  settings.action = items.truster_action;
+  settings.sendBucket = items.truster_sendBucket;
+  installListeners();
+});
+
+onTabRemoved = function(tabId) {
+  console.log('on tab removed');
+  delete tabs[tabId];
+}
+
+onTabReplaced = function(oldTabId, newTabId) {
+  console.log('on tab replaced');
+  delete tabs[oldTabId];
+  updateBadge(newTabId);
+}
+
+updateBadge = function(tab_id) {
+
+  if(tab_id in tabs) {
+    count = tabs[tab_id].size + "";
+  } else {
+    count = "";
+  }
+
+  chrome.browserAction.setBadgeText({
+    tabId : tab_id,
+    text : count
+  })
+}
+
+onBeforeRequest = function(info) {
+  // Check if request points to S3
+  var url = new URL(info.url);
+  var tab_id = info.tabId;
+  var hostname = url.hostname;
+  var TSTHRESHOLD = 1000*60*60; // 1 hour
+  var blacklist;
+  var downloadBlacklist = false;
+
+  if (hostname.endsWith("s3.amazonaws.com")){
+    console.log("[Truster] Checking " + info.url);
+
+    // Get blacklist
+    // Check if blacklist is stored
+    if (localStorage.getItem("truster-blacklist") != null){
+      console.log("[Truster] Blacklist is cached");
+      var storedObj = JSON.parse(localStorage.getItem("truster-blacklist"));
+      var currTimestamp = new Date().getTime();
+      if (currTimestamp - storedObj.ts < TSTHRESHOLD){
+        blacklist = storedObj.blacklist;
       } else{
+        // Cache expires
+        console.log("[Truster] Blacklist cached expired");
         downloadBlacklist = true;
       }
+    } else{
+      downloadBlacklist = true;
+    }
 
-      if (downloadBlacklist){
-        // Download blacklist
-        console.log("[Truster] Downloading blacklist");
-        // TODO timeout if endpoint does not respond threshold
-        var endpoint = "https://bucketsec.necst.it/api/blacklist";
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", endpoint, false);
-        xmlHttp.setRequestHeader("Cache-Control", "no-cache");
-        xmlHttp.send(null);
+    if (downloadBlacklist){
+      // Download blacklist
+      console.log("[Truster] Downloading blacklist");
+      // TODO timeout if endpoint does not respond threshold
+      var endpoint = "https://bucketsec.necst.it/api/blacklist";
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open("GET", endpoint, false);
+      xmlHttp.setRequestHeader("Cache-Control", "no-cache");
+      xmlHttp.send(null);
 
-        if (xmlHttp.status == 200){
-          blacklist = JSON.parse(xmlHttp.responseText);
-          var obj = {blacklist: blacklist, ts: new Date().getTime()}
-          localStorage.setItem("truster-blacklist", JSON.stringify(obj));
-        } else{
-          console.log("[Truster] Error downloading blacklist");
-        }
+      if (xmlHttp.status == 200){
+        blacklist = JSON.parse(xmlHttp.responseText);
+        var obj = {blacklist: blacklist, ts: new Date().getTime()}
+        localStorage.setItem("truster-blacklist", JSON.stringify(obj));
+      } else{
+        console.log("[Truster] Error downloading blacklist");
+      }
+    }
+
+    if (blacklist){
+      // Get bucket name
+      var re = /http(s)?:\/\/([a-zA-Z0-9.\-\_]+?).s3(-.*?)?.amazonaws.com\//;
+      var bucketName;
+
+      var match = re.exec(info.url)[2];
+      if (match != null) {
+        bucketName = match;
+      } else{
+        re = /http(s)?:\/\/s3(-.*?)?.amazonaws.com\/([a-zA-Z0-9.\-\_]+?)\//;
+        match = re.exec(info.url)[3];
+        bucketName = match;
       }
 
-      if (blacklist){
-        // Get bucket name
-        var re = /http(s)?:\/\/([a-zA-Z0-9.\-\_]+?).s3(-.*?)?.amazonaws.com\//;
-        var bucketName;
+      // Hash bucket name
+      var hashedBucketName = sha256(bucketName);
 
-        var match = re.exec(info.url)[2];
-        if (match != null) {
-          bucketName = match;
-        } else{
-          re = /http(s)?:\/\/s3(-.*?)?.amazonaws.com\/([a-zA-Z0-9.\-\_]+?)\//;
-          match = re.exec(info.url)[3];
-          bucketName = match;
+      // Check if hashed bucket name is in the blacklist.
+      if (blacklist.indexOf(hashedBucketName) > -1) {
+
+        try {
+          tabs[tab_id].add(bucketName);
+        } catch(TypeError) {
+          tabs[tab_id] = new Set();
+          tabs[tab_id].add(bucketName);
         }
+        updateBadge(tab_id);
 
-        // Hash bucket name
-        var hashedBucketName = sha256(bucketName);
+        if (settings.action == "block"){
+          console.log("[Truster] Untrusted resource blocked: " + info.url);
+          return {cancel: true};
 
-        // Get action to do
-        var action;
-        if (localStorage.getItem("truster-action") == null)
-          action = "block";
-        else
-          action = localStorage.getItem("truster-action");
+        } else if (settings.action == "warn"){
+          console.log("[Truster] Untrusted resource warning: " + info.url);
+          alert("Website is loading an untrusted resource: " + info.url);
 
-        // Check if hashed bucket name is in the blacklist.
-        if (blacklist.includes(hashedBucketName)){
-          if (action == "block"){
+        } else if (settings.action == "ask"){
+          console.log("[Truster] Untrusted resource, asking: " + info.url);
+          if (!confirm("Load an untrusted resource " + info.url + "?")){
             console.log("[Truster] Untrusted resource blocked: " + info.url);
-            return {redirectUrl: "javascript:"};
-
-          } else if (action == "warn"){
-            console.log("[Truster] Untrusted resource warning: " + info.url);
-            alert("Website is loading an untrusted resource: " + info.url);
-
-          } else if (action == "ask"){
-            console.log("[Truster] Untrusted resource, asking: " + info.url);
-            if (!confirm("Load an untrusted resource " + info.url + "?")){
-              console.log("[Truster] Untrusted resource blocked: " + info.url);
-              return {redirectUrl: "javascript:"};
-            }
+            return {cancel: true};
           }
-        } else {
-          var sendbucket;
-          if (localStorage.getItem("truster-sendbucket") == null)
-            sendbucket = false;
-          else
-            sendbucket = (localStorage.getItem("truster-sendbucket") == "true");
-
-          if (sendbucket){
-            console.log("[Truster] Sending bucket name for analysis");
-            var xmlhttp = new XMLHttpRequest();
-            var endpoint = "https://bucketsec.necst.it/api/scan";
-            xmlhttp.open("POST", endpoint, true);
-            xmlhttp.setRequestHeader("Content-Type", "application/json");
-            xmlhttp.send(JSON.stringify({bucket_name: bucketName}));
-          }
+        }
+      } else {
+        if (settings.sendbucket){
+          console.log("[Truster] Sending bucket name for analysis");
+          var xmlhttp = new XMLHttpRequest();
+          var endpoint = "https://bucketsec.necst.it/api/scan";
+          xmlhttp.open("POST", endpoint, true);
+          xmlhttp.setRequestHeader("Content-Type", "application/json");
+          xmlhttp.send(JSON.stringify({bucket_name: bucketName}));
         }
       }
     }
-  },
-  // filters
-  {
-    urls: ["<all_urls>"]
-  },
-  // extraInfoSpec
-  ["blocking"]);
-
+  }
+}
--- src/options.js
+++ src/options.js
@@ -1,9 +1,37 @@
-function setOptions() {
-    localStorage.setItem("truster-action", document.getElementById('action').value);
-    localStorage.setItem("truster-sendbucket", document.getElementById('sendbucket').checked);
-    alert("Saved");
+function getOptions() {
+    chrome.storage.sync.get({
+        truster_action : 'block',
+        truster_sendBucket : false
+    }, function(items) {
+        document.getElementById('action').value = items.truster_action;
+        document.getElementById('sendbucket').checked = items.truster_sendBucket;
+    });
+}
+
+function setOptions(evn) {
+    var action = document.getElementById('action').value;
+    var sendBucket = document.getElementById('sendbucket').checked;
+
+    chrome.storage.sync.set({
+        truster_action : action,
+        truster_sendBucket : sendBucket
+    }, function() {
+        var submit = document.getElementById('submit');
+        submit.textContent = 'Saved!';
+        submit.classList.add('btn-success');
+        submit.classList.remove('btn-default');
+        setTimeout(function() {
+            submit.textContent = 'Save';
+            submit.classList.remove('btn-success');
+            submit.classList.add('btn-default');
+        }, 1500);
+    });
+
+    evn.preventDefault();
+    evn.stopPropagation();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("button").addEventListener("click", setOptions);
+    getOptions();
+    document.querySelector("button").addEventListener("click", setOptions);
 });
